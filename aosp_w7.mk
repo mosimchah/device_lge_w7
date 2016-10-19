45288babced64096a672f8d082b2c5a3519e260e
mosimchah <mosimchah@gmail.com>
2016-10-18 20:03:53 -0400

    AOSPify, and optimize

--- aosp_w7.mk
+++ aosp_w7.mk
@@ -1,5 +1,5 @@
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+# Inherit some common aosp stuff.
+$(call inherit-product, vendor/aosp/common.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/lge/w7/device_w7.mk)
 
@@ -9,8 +9,10 @@ $(call inherit-product, device/lge/w7/device_w7.mk)
 
 # Release name
 PRODUCT_RELEASE_NAME := LG L90
-PRODUCT_NAME := cm_w7
+PRODUCT_NAME := aosp_w7
 PRODUCT_MODEL := w7
 PRODUCT_DEVICE := w7
 PRODUCT_MANUFACTURER := LGE
 PRODUCT_BRAND := lge
+
+PRODUCT_GMS_CLIENTID_BASE := android-lge
